172175cd6d6dc2457e6f540d3a1426b97afb2518
jessicabrinegar <jessicabrinegar@outlook.com>
2023-08-16 13:49:49 -0600

    removed unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,7 @@ import { TransactionsModule } from './transactions/transactions.module';
 // import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { config } from './database/config/ormconfig';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
-// import { Account } from './accounts/models/account.entity';
-// import { Transaction } from './transactions/models/transaction.entity';
-// import { AuditTrail } from './audit-trails/models/audit.entity';
 
 @Module({
   imports: [DatabaseModule, AccountsModule, TransactionsModule],
