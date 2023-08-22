3a4910a45dbdf7af9cbb4a650f2c955325182eec
jessicabrinegar <jessicabrinegar@outlook.com>
2023-08-22 16:54:42 -0600

    audit trail & event emitters setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransactionsModule } from './transactions/transactions.module';
-// import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { AuditTrailModule } from './audit-trails/audit-trails.module';
 
 @Module({
-  imports: [DatabaseModule, AccountsModule, TransactionsModule],
+  imports: [DatabaseModule, EventEmitterModule.forRoot(), AccountsModule, TransactionsModule, AuditTrailModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+//forRoot() initializes the event emitter & registers any declarative
+// event listeners that exist within the app
--- src/audit-trails/audit-trails.module.ts
+++ src/audit-trails/audit-trails.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuditTrail } from './models/audit.entity';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AuditTrailRepository } from './audit-trail.repository';
+import { AuditTrailController } from './audit-trails.controller';
+import { AuditTrailService } from './audit-trails.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([AuditTrail])],
-    controllers: [],
-    providers: [],
+    imports: [TypeOrmModule.forFeature([AuditTrail, AuditTrailRepository]), EventEmitterModule.forRoot()],
+    controllers: [AuditTrailController],
+    providers: [AuditTrailService],
+    exports: [AuditTrailService]
 })
 export class AuditTrailModule {}
